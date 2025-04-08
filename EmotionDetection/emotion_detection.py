ec7793fb889a758a2b585f8fb4f4854fd17ae039
Bgtti <bru.mgtti@gmail.com>
2025-04-08 10:18:43 -0400

    final

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers=header)
+    response_dict = json.loads(response.text)
+
+    if response.status_code == 200:
+        emotion_scores = response_dict['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+        func_output = {
+        'anger': emotion_scores['anger'],
+        'disgust': emotion_scores['disgust'],
+        'fear': emotion_scores['fear'],
+        'joy': emotion_scores['joy'],
+        'sadness': emotion_scores['sadness'],
+        'dominant_emotion': dominant_emotion
+        }
+        return func_output
+
+    elif response.status_code == 400:
+        error_obj = {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+        }
+        return error_obj
+    
